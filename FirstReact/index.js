c176128cd5f20555fdfd3e8310afb1c3e54349c8
Lucas Tito <lucassga500@gmail.com>
2023-02-23 11:46:49 -0300

    first commit

---
+++ FirstReact/index.js
@@ -0,0 +1,6 @@
+
+/* the function "render" has two parameters,
+ * the first being the html code to render
+ * second, where the html code will be rendered
+*/
+ReactDOM.render(<h2>I'm being rendered using react!</h2>, document.querySelector("#root"))
