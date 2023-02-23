92753b9146ef3a338033d0fdbb7bc2f1cd8259ff
Lucas Tito <lucassga500@gmail.com>
2023-02-23 14:23:55 -0300

    small touches on hello world

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -6,6 +6,17 @@
 ReactDOM.render(
     <ul>
         <li>React can be used to write non-static web pages</li>
-        <li>React is composable</li>
+        <li>
+        <details>
+                <summary>React is composable</summary>
+                <p>You can create your own html elements, which helps to reduce html code</p>
+            </details>
+        </li>
+        <li>
+            <details>
+                <summary>It's Declarative</summary>
+                <p>React simplifies it's code with it's built in functions</p>
+            </details>
+        </li>
     </ul>,
     document.querySelector("#root"))
