ccfd250d85d3087577372431134bd396e2e8db8d
Lucas Tito <lucassga500@gmail.com>
2023-02-23 21:40:32 -0300

    stopped using cdns imports

---
+++ 3_goodbye_cdns/src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+    <div align="center">
+        <h1>Hi</h1>
+        <p>From now on I stopped using CDNs, which are those import that we were using previously</p>
+        <p><h5>this method that I'm using now uses npx from node</h5></p>
+    </div>
+    ,document.querySelector("#root"))
