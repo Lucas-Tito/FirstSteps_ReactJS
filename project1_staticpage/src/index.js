aef507516d7b847f54419da40314d534897d1615
Lucas-Tito <lucassga500@gmail.com>
2023-02-27 15:25:52 -0300

    started composable studies

--- project1_staticpage/src/index.js
+++ project1_staticpage/src/index.js
@@ -1,19 +1,48 @@
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom"
 import "./index.css"
 
-const pageContent = (
-    <div>
-    {/* without require and default the img doesn't render for some reason */}
-    <img src={require('./images/react.svg').default} className="logo"/>
 
-    <h1>Fun facts about React</h1>
-    <ul>
-        <li>Was first released in 2013</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-    </div>
+function Header(){
+    return (
+    <header>
+        <div className="logoContainer">
+            <img className="logo" src={require("./images/react.svg").default}/> 
+            <span>ReactFacts</span>
+        </div>
+        <nav></nav>
+    </header>
+    )
+}
+
+function UnorderedList(){
+    return(
+        <div className="UnorderedList">
+            <h1>Reason I'm excited about React</h1>
+            <ul>
+                <li>React is very hireable</li>
+                <li>React is composable</li>
+                <li>React is declarative</li>
+                <li>React code looks clean and fresh!</li>
+            </ul>
+        </div>
+    )
+}
+
+function Footer(){
+    return (
+        <footer>
+            <div className="footerContent">
+                <p className="message">God bless those in need</p>
+                <p>Copyright &copy; 2023<span> Lucas Tito </span>development. All rights reserved.</p>
+            </div>
+        </footer>
+    )
+}
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+    <>
+        <Header/>
+        <UnorderedList/>
+        <Footer/>
+    </>
 )
-
-ReactDOM.createRoot(document.querySelector("#root")).render(pageContent)
