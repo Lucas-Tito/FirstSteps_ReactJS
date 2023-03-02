f9f91fecdde7305fd771965a9430eff04556f0d4
Lucas-Tito <lucassga500@gmail.com>
2023-03-01 21:29:23 -0300

    finished overall project

--- Projects/2_businesscard/src/components/Card_Footer.js
+++ Projects/2_businesscard/src/components/Card_Footer.js
@@ -1,11 +1,27 @@
+import React from "react"
 import linkedin_icon from "../images/linkedin_icon_footer.svg"
 import github_icon from "../images/github_icon.svg"
+
+
 
 export default function Card_Footer(){
     return(
         <div className="footer_container">
-            <img id="github_icon" src={github_icon}/>
-            <img id="linkedin_icon" src={linkedin_icon}/>
+            <img id="github_icon" src={github_icon} onClick={setup_github_link()}/>
+            <img id="linkedin_icon" src={linkedin_icon} onClick={setup_linkedin_link()}/>
         </div>
     )
+}
+
+
+function setup_github_link(){
+    return function(){
+        window.location.href = ("https://github.com/Lucas-Tito")
+    }
+}
+
+function setup_linkedin_link(){
+    return function(){
+        window.location.href = ("https://www.linkedin.com/in/lucas-tito-530997211/")
+    }
 }
--- Projects/2_businesscard/src/components/Contact_Btns.js
+++ Projects/2_businesscard/src/components/Contact_Btns.js
@@ -1,18 +1,34 @@
+import React from "react"
 import email_icon from "../images/email_icon.svg"
 import linkedin_icon from "../images/linkedin_icon.svg"
 
 export default function Contact_Btns(){
     return(
         <div className="contact_btns">
-            <button id="email" href="">
-                <img id="email_icon" src={email_icon}/>
+            <button id="email" onClick={setup_email_link()}>
+                <img id="email_btn_icon" src={email_icon}/>
                 <span>Email</span>
             </button>
-
-            <button id="linkedin" href="">
-                <img id="linkedin_icon" src={linkedin_icon}/>
+           
+            <button id="linkedin" onClick={setup_linkedin_link()}>
+                <img id="linkedin_btn_icon" src={linkedin_icon}/>
                 <span>LinkedIn</span>
             </button>
         </div>
     )
 }
+
+function setup_email_link(){
+    return function(){
+        window.location.href = ("mailto:lucastito.contato@gmail.com")
+    }
+}
+
+function setup_linkedin_link(){
+    return function(){
+        window.location.href = ("https://www.linkedin.com/in/lucas-tito-530997211/")
+    }
+}
+    
+
+
