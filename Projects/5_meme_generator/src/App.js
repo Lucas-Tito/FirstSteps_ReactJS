b2191c093aa39b4e71e53a67ca264394c5047bdc
Lucas-Tito <lucassga500@gmail.com>
2023-03-08 07:53:28 -0300

    now react is updating meme image

--- Projects/5_meme_generator/src/App.js
+++ Projects/5_meme_generator/src/App.js
@@ -1,13 +1,11 @@
 import Form from "./components/Form";
 import Header from "./components/Header";
-import Meme_img from "./components/Meme_img";
 
 export default function App(){
     return(
         <>
             <Header/>
             <Form/>
-            <Meme_img/>
         </>
     )
 }
--- Projects/5_meme_generator/src/components/Form.js
+++ Projects/5_meme_generator/src/components/Form.js
@@ -1,18 +1,26 @@
 import MemeGenerator from "../utils/MemeGenerator"
+import meme_placeholder from "../images/meme_placeholder.png"
+import React from "react"
 
 export default function Form(){
+    const [randomMeme_img, setRandomMeme] = React.useState(meme_placeholder)
+    
     function handleClick(){
-        let randomMeme_img = new MemeGenerator().getRandomMeme_img()
-        console.log(randomMeme_img)
+        setRandomMeme(new MemeGenerator().getRandomMeme_img())
     }
 
     return(
-        <div className="meme_form">
-            <div className="input_container">
-                <input placeholder="Top text"></input>
-                <input placeholder="Bottom text"></input>
+        <>
+            <div className="meme_form">
+                <div className="input_container">
+                    <input placeholder="Top text"></input>
+                    <input placeholder="Bottom text"></input>
+                </div>
+                <button onClick={handleClick}>Get a new meme image 🖼️</button>
             </div>
-            <button onClick={handleClick}>Get a new meme image 🖼️</button>
-        </div>
+            <div className="meme_container">
+                <img src={randomMeme_img}/>
+            </div>
+        </>
     )
 }
