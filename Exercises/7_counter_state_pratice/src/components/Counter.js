f1541ae62e34b7fbc30e218597cfa000802e88e2
Lucas-Tito <lucassga500@gmail.com>
2023-03-07 21:51:00 -0300

    state exercise finished

---
+++ Exercises/7_counter_state_pratice/src/components/Counter.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+export default function Counter(){
+    let [counter, setCounter] = React.useState(0)
+    
+    function add(){
+        setCounter(++counter)
+    }
+
+    function reduce(){
+        setCounter(--counter)
+    }
+
+    return(
+        <div className="counter_container">
+            <div className="display">
+                <spam>{counter}</spam>
+            </div>
+            <button className="reduce_btn" onClick={reduce}>-</button>
+            <button className="add_btn" onClick={add}>+</button>
+        </div>
+    )
+}
+    
+    
