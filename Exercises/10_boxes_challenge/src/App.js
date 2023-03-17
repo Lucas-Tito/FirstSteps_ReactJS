30b8f3ee7e5f52bb386c5cfbd2d308dfed3d970a
Lucas Tito <lucassga500@gmail.com>
2023-03-17 16:42:51 -0300

    finished first part of boxes challenge

--- Exercises/10_boxes_challenge/src/App.js
+++ Exercises/10_boxes_challenge/src/App.js
@@ -1,7 +1,23 @@
+import boxesData from "./boxesData"
+import Box from "./components/Box"
+
 export default function App(){
+
+    let box_elements = boxesData.map(function(box){
+        return(
+            <Box
+                key={box.id}
+                data={box}
+            />
+        )
+    })
+
     return(
         <>
         <h1>Boxes will go here</h1>
+        <div className="boxes_container">
+            {box_elements}
+        </div>
         </>
     )
 }
--- Exercises/10_boxes_challenge/src/components/Box.jsx
+++ Exercises/10_boxes_challenge/src/components/Box.jsx
@@ -1,6 +1,13 @@
-export default function Box(){
+export default function Box(props){
+
+    function handleClick(){
+        let box_containers = document.querySelectorAll(".box_container")
+        box_containers[props.data.id-1].classList.toggle("on")   
+    }
+
     return(
-        <>
-        </>
+        <button className="box_container" onClick={handleClick}>
+
+        </button>
     )
 }
