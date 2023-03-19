c0de60c9f4b834abb3b2c2110e49a26c11f5d2ae
Lucas-Tito <lucassga500@gmail.com>
2023-03-18 22:05:24 -0300

    part 4 of boxes challenge complete

--- Exercises/10_boxes_challenge/src/App.js
+++ Exercises/10_boxes_challenge/src/App.js
@@ -1,13 +1,37 @@
 import boxesData from "./boxesData"
 import Box from "./components/Box"
+import { useState } from "react"
 
 export default function App(){
 
-    let box_elements = boxesData.map(function(box){
+    const [boxes, setBoxes] = useState(boxesData)
+
+    function toggleOn(id){
+        // code run trough current boxes array and find the box that matches the id in the parameter, thant updates it
+        // then that change is stored in the newBoxes array
+        setBoxes((prevBoxes) => {
+            let newBoxes = []
+
+            prevBoxes.forEach(box => {
+                if(box.id === id)
+                    newBoxes.push({
+                        ...box,
+                        on:!box.on
+                    })
+                else
+                    newBoxes.push(box)
+            })
+
+            return newBoxes
+        })
+    }
+
+    let box_elements = boxes.map(function(box){
         return(
             <Box
                 key={box.id}
                 data={box}
+                toggleOn={toggleOn}
             />
         )
     })
--- Exercises/10_boxes_challenge/src/components/Box.jsx
+++ Exercises/10_boxes_challenge/src/components/Box.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react"
+
 export default function Box(props){
 
-    function handleClick(){
-        let box_containers = document.querySelectorAll(".box_container")
-        box_containers[props.data.id-1].classList.toggle("on")   
+    var styles = {
+        backgroundColor: "transparent"
     }
 
+    if(props.data.on){
+        var styles = {
+            backgroundColor: "grey"
+        }
+    }
+
+    
+
     return(
-        <button className="box_container" onClick={handleClick}>
+        // you have to use the onClick like that to pass a parameter
+        <button className="box_container" onClick={()=>props.toggleOn(props.data.id)} style={styles}>
 
         </button>
     )
