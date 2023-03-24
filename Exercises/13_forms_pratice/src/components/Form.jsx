9e5bee8329b427ef94f2eda4c23ff9bb59e5dba3
Lucas Tito <lucassga500@gmail.com>
2023-03-24 14:53:46 -0300

    finished forms pratice

---
+++ Exercises/13_forms_pratice/src/components/Form.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react"
+
+export default function Form(){
+    const [formData, setFormData] = useState(
+        {
+            email:"",
+            password:"",
+            confirmPassword:"",
+            joinedNews:false
+        })
+
+    function handleChange(event){
+        const {name, value, type, checked} = event.target
+
+        setFormData((prevFormData)=>{
+            return {
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        if(formData.password !== formData.confirmPassword)
+            console.log("password doesn't match")
+        else{
+            console.log("Welcome")
+
+            if(formData.joinedNews)
+                console.log("Thanks for signing for the newsletter!")
+        }
+            
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <input
+                name="email"
+                type="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+            />
+
+            <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+            />
+
+            <input
+                name="confirmPassword"
+                type="password"
+                placeholder="Confirm Password"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+            />
+
+            <div className="checkbox">
+                <input
+                    id="joinNews"
+                    type="checkbox"
+                    name="joinNews"
+                    checked={formData.joinedNews}
+                    onChange={handleChange}
+                />
+
+                <label htmlFor="joinNews">I want to join the newsletter</label>
+            </div>
+
+            <button>Sign Up</button>
+        </form>
+    )
+}
